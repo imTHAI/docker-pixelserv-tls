6255b6c8c102be2b2aa62fb4c99f168abb81440f
imTHAI <36070606+imTHAI@users.noreply.github.com>
2022-02-05 15:52:43 +0100

    Update docker-entrypoint.sh

--- root/etc/init.d/docker-entrypoint.sh
+++ root/etc/init.d/docker-entrypoint.sh
@@ -1,9 +1,7 @@
 #! /bin/sh
-echo "pouet1"
 # Generating the certificates:
 if ! [[ $(find /var/cache/pixelserv -maxdepth 1 -name 'ca.*' -print -quit) ]];
 then
-  echo "pouet2"
   apk add easy-rsa
   cd /tmp
   /usr/share/easy-rsa/easyrsa init-pki
